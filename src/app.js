21d7600523836a68cf706837f88e98e0f7962238
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-19 13:43:44 -0300

    Rota transações

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
 import Joi from "joi";
+import bcrypt from "bcrypt"
+import {v4 as uuid} from "uuid"
 
 dotenv.config()
 
@@ -23,16 +25,60 @@ try {
 const db = mongoClient.db()
 
 const schemaSignUp = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().required().min(1),
 
-    email: Joi.string().email().required(),
+    email: Joi.string().email().required().min(1),
 
     password: Joi.string().min(3).required(),
 
-    confirmPassword: Joi.ref('password')
-
 })
 
+const schemaLogin = Joi.object({
+    email: Joi.string().email().required().min(1),
+    password: Joi.string().min(3).required()
+})
+
+const schemaTransacao = Joi.object({
+    valor: Joi.number().positive().required(),
+    descricao: Joi.string().required()
+})
+
+const alt = Joi.alternatives().try("entrada","saida")
+const validToken = Joi.string().length(36)
+
+app.post("/",async (req,res)=> {
+
+    const {error} = schemaLogin.validate(req.body)
+
+    if(error !== undefined){
+        return res.status(422).send(error.details[0].message)
+    }
+
+    const {email, password} = req.body
+
+    try{
+        const [user] = await db.collection("users").find({email}).toArray()
+
+        if(user === undefined) return res.status(404).send("Email não cadastrado")
+
+        if(!bcrypt.compareSync(password, user.password)) return res.status(401).send("senha incorreta")
+        
+        const token = uuid()
+
+        await db.collection("sessions").insertOne({token: token, userId: user._id})
+
+        console.log(token.length)
+        
+        return res.status(200).send(token)
+        
+    } catch(err){
+        return res.status(500).send(err)
+    }
+
+
+
+    
+})
 
 app.post("/cadastro",async (req,res)=>{
 
@@ -51,7 +97,10 @@ app.post("/cadastro",async (req,res)=>{
             return res.status(409).send("o email já está sendo utilizado")
         }
 
-        await db.collection("users").insertOne(req.body)
+        const {name,email} = req.body
+        const password = bcrypt.hashSync(req.body.password,10)
+
+        await db.collection("users").insertOne({name,email,password})
 
         return res.status(201).send("ok")
 
@@ -63,5 +112,50 @@ app.post("/cadastro",async (req,res)=>{
     
 
 })
+
+app.post("/nova-transacao/:tipo",async (req,res)=>{
+
+    const { token } = req.headers
+    const { tipo } = req.params
+
+
+    if(token === undefined){
+        return res.status(401).send("Token missing")
+    }
+
+    if(alt.validate(tipo).error){
+        return res.status(422).send(alt.validate(tipo).error.details[0].message)
+    }
+
+    if(validToken.validate(token).error){
+
+        return res.status(422).send(validToken.validate(token).error.details[0].message)
+    }
+
+    if(schemaTransacao.validate(req.body).error){
+        console.log(schemaTransacao.validate(req.body).error)
+
+        return res.status(422).send(schemaTransacao.validate(req.body).error.details[0].message)
+    }
+
+
+    try{
+        //verificar se existe usuario com o token
+        const [user] = await db.collection("sessions").find({token}).toArray()
+
+        if(user === undefined) return res.status(422).send("Token inválido")
+
+        await db.collection("transacoes").insertOne({userId: user.userId, tipo, valor: req.body.valor, descricao: req.body.descricao })
+
+        return res.send("ok")
+
+    } catch(err){
+        return res.status(500).send("ok")
+    }
+    
+
+    
+    
+ })
 
 app.listen(PORT, ()=>{console.log(`rodando na porta ${PORT}`)})
